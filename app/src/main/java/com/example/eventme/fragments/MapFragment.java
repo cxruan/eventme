3728fbdc6091b06c243f3172be73f0cd89cefd16
therealnemis <simonzhang901@gmail.com>
2022-11-01 00:33:41 -0700

    added getting events from firebase and filter by distance

--- app/src/main/java/com/example/eventme/fragments/MapFragment.java
+++ app/src/main/java/com/example/eventme/fragments/MapFragment.java
@@ -10,11 +10,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.eventme.databinding.FragmentMapBinding;
+import com.example.eventme.models.Event;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -25,12 +27,15 @@ public class MapFragment extends Fragment {
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
+    private ArrayList<Event> allEvents = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentMapBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
+
     }
 
     @Override
@@ -38,6 +43,8 @@ public class MapFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("events");
         mAuth = FirebaseAuth.getInstance();
+        getAllEvents();
+
     }
 
     public void getAllEvents(){
@@ -46,7 +53,12 @@ public class MapFragment extends Fragment {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         //Get map of users in datasnapshot
-                        collectEventDetails((Map<String,Object>) dataSnapshot.getValue());
+                        GenericTypeIndicator<ArrayList<Event>> t = new GenericTypeIndicator<ArrayList<Event>>() {};
+                        for(DataSnapshot ds: dataSnapshot.getChildren()){
+                            Event event = ds.getValue(Event.class);
+                            allEvents.add(event);
+                        }
+                        isWithinDistance(10, 51.5007, 0.1246);
                     }
 
                     @Override
@@ -55,20 +67,33 @@ public class MapFragment extends Fragment {
                     }
                 });
     }
-    private void collectEventDetails(Map<String,Object> users) {
 
-        ArrayList<Long> phoneNumbers = new ArrayList<>();
+    public void isWithinDistance(int kilometers, double currentLon, double currentLat){
+        System.out.println("yeahyeahyeah");
+        ArrayList<Event> eventsInDistance = new ArrayList<>();
+        for(Event event : allEvents){
+            double eventLon = event.getGeoLocation().get("lng");
+            double eventLat = event.getGeoLocation().get("lat");
 
-        //iterate through each user, ignoring their UID
-        for (Map.Entry<String, Object> entry : users.entrySet()){
+            double dLat = Math.toRadians(currentLat - eventLat);
+            double dLon = Math.toRadians(currentLon - eventLon);
 
-            //Get user map
-            Map singleUser = (Map) entry.getValue();
-            //Get phone field and append to list
-            phoneNumbers.add((Long) singleUser.get("phone"));
+            currentLat = Math.toRadians(currentLat);
+            eventLat = Math.toRadians(eventLat);
+
+            double a = Math.pow(Math.sin(dLat / 2), 2) +
+                    Math.pow(Math.sin(dLon / 2), 2) *
+                            Math.cos(eventLat) *
+                            Math.cos(currentLat);
+            double rad = 6371;
+            double c = 2 * Math.asin(Math.sqrt(a));
+            double distance = rad * c;
+            System.out.println(event.getName());
+            System.out.println(distance);
+            if (distance < kilometers){
+                eventsInDistance.add(event);
+            }
         }
-
-        System.out.println(phoneNumbers.toString());
+        allEvents = eventsInDistance;
     }
-
 }
