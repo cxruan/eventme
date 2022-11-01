ec1289db8a39fa5ee1a94a6ce24acbf73f1c82d4
therealnemis <simonzhang901@gmail.com>
2022-10-31 23:40:07 -0700

    in middle of get request

--- app/src/main/java/com/example/eventme/fragments/MapFragment.java
+++ app/src/main/java/com/example/eventme/fragments/MapFragment.java
@@ -10,9 +10,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.eventme.databinding.FragmentMapBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MapFragment extends Fragment {
     private FragmentMapBinding binding;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     @Nullable
     @Override
@@ -20,4 +32,43 @@ public class MapFragment extends Fragment {
         binding = FragmentMapBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("events");
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void getAllEvents(){
+        mDatabase.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        collectEventDetails((Map<String,Object>) dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
+    }
+    private void collectEventDetails(Map<String,Object> users) {
+
+        ArrayList<Long> phoneNumbers = new ArrayList<>();
+
+        //iterate through each user, ignoring their UID
+        for (Map.Entry<String, Object> entry : users.entrySet()){
+
+            //Get user map
+            Map singleUser = (Map) entry.getValue();
+            //Get phone field and append to list
+            phoneNumbers.add((Long) singleUser.get("phone"));
+        }
+
+        System.out.println(phoneNumbers.toString());
+    }
+
 }
